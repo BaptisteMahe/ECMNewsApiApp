1d5f2587d46905f3e53ad3d42153f563b9a532c7
BaptisteMahe <mahe.baptiste.19@gmail.com>
2020-12-04 15:39:02 +0100

    Can get the API

--- app/src/main/java/fr/centrale/newsapiapp/MainActivity.kt
+++ app/src/main/java/fr/centrale/newsapiapp/MainActivity.kt
@@ -2,10 +2,40 @@ package fr.centrale.newsapiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.android.volley.Request
+import com.android.volley.toolbox.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testApiRequestButton = findViewById<Button>(R.id.testApiRequest)
+
+        testApiRequestButton.setOnClickListener{
+            val queue = Volley.newRequestQueue(this)
+            val url = "https://newsapi.org/v2/sources?apiKey=d31f5fa5f03443dd8a1b9e3fde92ec34&language=fr"
+
+            val stringRequest = object: JsonObjectRequest(
+                Request.Method.GET, url, null,
+                { response ->
+                    Log.d("TAG", response.toString())
+                },
+                { error ->
+                    Log.d("TAG", "Something went wrong: $error") })
+            {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["User-Agent"] = "Mozilla/5.0"
+                    return headers
+                }
+            }
+
+            queue.add(stringRequest)
+        }
+
     }
 }
